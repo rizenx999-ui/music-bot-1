ee56d03d3734de625583d8284301e77799dc26ec
rizenx999-ui <rizenx999@gmail.com>
2026-06-24 05:06:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Bot starting...");
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds
+  ]
+});
+
+client.once("ready", () => {
+  console.log("Bot is ONLINE!");
+});
+
+client.login(process.env.TOKEN);
