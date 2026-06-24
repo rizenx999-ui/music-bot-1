43797983e12aee069d13d503735e71a24bcf0ef0
rizenx999-ui <rizenx999@gmail.com>
2026-06-24 06:13:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
-const { EmbedBuilder } = require("discord.js");
-
-// video details (after yt-search or url)
-const embed = new EmbedBuilder()
-  .setTitle(video.title)
-  .setURL(video.url)
-  .setAuthor({ name: video.author.name })
-  .setThumbnail(video.thumbnail)
-  .addFields(
-    { name: "⏱ Duration", value: video.timestamp || "Live", inline: true },
-    { name: "👤 Requested by", value: interaction.user.tag, inline: true }
-  )
-  .setColor("Blue");
-
-interaction.reply({ embeds: [embed] });
+const result = await ytSearch(query);
+const video = result.videos[0];
